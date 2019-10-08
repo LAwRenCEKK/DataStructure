06710e8fdb50a85e09e8163211ce607428d32ac9
JIANPENG LIU <lawrence@Lawrence-2081.local>
2019-10-08 00:02:44 -0400

    Add complexity analysis

--- quickSort.java
+++ quickSort.java
@@ -1,13 +1,13 @@
 
-// Quick sort
+import java.util.Random;
 
 public class quickSort{
-	static int[] arr = new int[] {1,2,3,4,7,6,5,9,8,10};
+	static int[] arr;
+	static int counter;
 
     static void quicksort(int low, int high){
 		if (low < high){// determine if it is time to terminate the branch
 			int pivot = partition(low,high);
-			
 			//recursion part
 			quicksort(low,pivot-1); //left part of the pivot
 			quicksort(pivot+1,high);//right part of the pivot
@@ -24,8 +24,10 @@ public class quickSort{
 				i++;
 				int t = arr[i];
 				arr[i] = arr[j];
-				arr[j] = arr[i];
+				arr[j] = t;
+				counter++;
 				}
+			counter++;
 			}
 		int t = arr[i+1];
 		arr[i+1] = pivot;
@@ -34,8 +36,21 @@ public class quickSort{
 	}
 
     public static void main(String args[]) {
+    	Random rand = new Random();
+    	counter = 0;
+		int size_a = Integer.parseInt(args[0]);
+		arr = new int[size_a];
+		for (int i=0;i<size_a;i++){
+			arr[i] = rand.nextInt(50);
+		}
+		quicksort(0,size_a-1);
 		System.out.println("This is quick sort!");
-		quicksort(0,9);
-		for(int i:arr){System.out.println(i);}
+		System.out.printf("Actual Operation %d\n", counter);
+		System.out.printf("Worst case %d\n", (size_a-1)*size_a);
+		System.out.printf("Average case %f\n", (size_a)*Math.log(size_a));
+
+
+
+//		for(int i:arr){System.out.println(i);}
 	}
 }
